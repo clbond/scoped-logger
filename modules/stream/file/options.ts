7f3bab3eed7c22d7bcf1323427a6fe98660e0903
Christopher Bond <bond@Bond-MacBook.local>
2017-02-08 12:09:58 -0500

    Make options optional

--- modules/stream/file/options.ts
+++ modules/stream/file/options.ts
@@ -1,13 +1,13 @@
 export interface RollingOptions {
   // Maximum size of a file before it is rolled
-  thresholdSize: number;
+  thresholdSize?: number;
 
   // Number of files to keep before deletion of oldest files
-  retention: number;
+  retention?: number;
 
   // The folder which we will write our log files to
-  path: string;
+  path?: string;
 
   // Filename templates for current and previous files
-  filenames: {current: string, previous: string};
+  filenames?: {current: string, previous: string};
 }
